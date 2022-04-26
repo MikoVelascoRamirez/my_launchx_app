465d1946d6e59306c1240f5f6c5e0aad89732d95
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-04-26 17:01:01 -0500

    refactor: changing test beta to test for mc class

--- test/missionCommander.test.js
+++ test/missionCommander.test.js
@@ -1,8 +1,8 @@
-const missionCommander = require('../app/missionCommander');
+const MissionCommander = require('../app/missionCommander');
 
-describe("Esto es una suite de pruebas", () => {
-  test("Caso de prueba 1", () => {
-    const result = 1 + 2;
-    expect(result).toBe(3);
+describe("Unit Tests for Mission Commander Class", () => {
+  test("1) Create a mission commander object", () => {
+    const mc = new MissionCommander("Woopa");
+    expect(mc.name).toBe("Woopa");
   });
 });
