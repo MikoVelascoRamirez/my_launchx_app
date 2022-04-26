02c29a1e32ab514bcf185feae0012255d02f0589
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-04-26 16:57:11 -0500

    feat: importing mc module

--- test/missionCommander.test.js
+++ test/missionCommander.test.js
@@ -1,6 +1,8 @@
+const missionCommander = require('../app/missionCommander');
+
 describe("Esto es una suite de pruebas", () => {
   test("Caso de prueba 1", () => {
     const result = 1 + 2;
-    expect(result).toBe(10);
+    expect(result).toBe(3);
   });
 });
